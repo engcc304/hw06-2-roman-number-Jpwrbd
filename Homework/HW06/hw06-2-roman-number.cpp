0a67ad6ade7a994a79acad9af88927b3f343ba23
Jpwrbd <jpwrbd456@gmail.com>
2023-08-04 01:56:39 +0700

    upcocoa

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -26,28 +26,26 @@
 
 #include <stdio.h>
 
-void convertToRoman(int num) {
+void convertToRoman(int num)
+{
     int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char* romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
+    char *romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     printf("%d = ", num);
-
-    for (int i = 0; num > 0; i++) {
-        while (num >= values[i]) {
+    for (int i = 0; num > 0; i++)
+    {
+        while (num >= values[i])
+        {
             printf("%s", romanNumerals[i]);
             num -= values[i];
         }
     }
-
     printf("\n");
 }
-
-int main() {
+int main()
+{
     int number;
-    printf("Enter a number: ");
+    printf("Test case: ");
     scanf("%d", &number);
-
     convertToRoman(number);
-
     return 0;
 }
