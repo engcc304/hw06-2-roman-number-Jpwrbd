2475111c854226d948ef1ffe63de965c0fd00fe9
Jpwrbd <jpwrbd456@gmail.com>
2023-08-03 18:32:55 +0700

    test

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -23,3 +23,30 @@
         929 = CMXXIX
 
 */
+#include <stdio.h>
+#include <string.h>
+
+void convertToRoman(int num) {
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char* romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    char romanNumeral[100];
+    memset(romanNumeral, '\0', sizeof(romanNumeral));
+    
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            strcat(romanNumeral, romanNumerals[i]);
+            num -= values[i];
+        }
+    }
+    
+    printf("%d = %s\n", num, romanNumeral);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    convertToRoman(num);
+    return 0;
+}
+
