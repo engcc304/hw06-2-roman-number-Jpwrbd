8d6795c955a12be0305e06d387efc22553d165c0
Jpwrbd <jpwrbd456@gmail.com>
2023-08-03 18:35:17 +0700

    test

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -30,15 +30,15 @@ void convertToRoman(int num) {
     int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     char* romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     char romanNumeral[100];
-    memset(romanNumeral, '\0', sizeof(romanNumeral));
-    
+    memset(romanNumeral, '\0', sizeof(romanNumeral)); // Initialize with null characters
+
     for (int i = 0; i < 13; i++) {
         while (num >= values[i]) {
             strcat(romanNumeral, romanNumerals[i]);
             num -= values[i];
         }
     }
-    
+
     printf("%d = %s\n", num, romanNumeral);
 }
 
@@ -49,4 +49,3 @@ int main() {
     convertToRoman(num);
     return 0;
 }
-
